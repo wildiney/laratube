ad6f3c65838ffc558b466dc00341b43b393e5c50
Wildiney Di Masi <wildiney@gmail.com>
2025-01-19 13:17:40 -0300

    refactor: remove unused dependencies and clean up configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/laratube/',
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'LaraTube',
-        short_name: 'LaraTube',
-        description: 'Videos selecionados cuidadosamente para você',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    })
+    react()
   ],
 })
